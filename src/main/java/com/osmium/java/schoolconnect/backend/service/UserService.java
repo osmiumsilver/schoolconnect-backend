ad7756bd3592a312b2afdd57160e4cb135244ada
ZL <1351916203@qq.com>
2022-09-16 21:40:08 +0800

    mybatis-plus-join 集成修改

--- src/main/java/com/osmium/java/schoolconnect/backend/service/UserService.java
+++ src/main/java/com/osmium/java/schoolconnect/backend/service/UserService.java
@@ -2,15 +2,12 @@ package com.osmium.java.schoolconnect.backend.service;
 
 import com.github.yulichang.base.MPJBaseService;
 import com.osmium.java.schoolconnect.backend.entity.User;
-import com.osmium.java.schoolconnect.backend.mapper.UserMapper;
-
-import javax.servlet.http.HttpSession;
 
 /**
  * @author
  * @date 2022/3/29
  * @apinote
  */
-public interface UserService extends MPJBaseService<UserMapper> {
+public interface UserService extends MPJBaseService<User> {
 
 }
--- src/main/java/com/osmium/java/schoolconnect/backend/service/impl/UserServiceImpl.java
+++ src/main/java/com/osmium/java/schoolconnect/backend/service/impl/UserServiceImpl.java
@@ -1,17 +1,10 @@
 package com.osmium.java.schoolconnect.backend.service.impl;
 
 import com.github.yulichang.base.MPJBaseServiceImpl;
-import com.osmium.java.schoolconnect.backend.entity.SysUser;
 import com.osmium.java.schoolconnect.backend.entity.User;
 import com.osmium.java.schoolconnect.backend.mapper.UserMapper;
 import com.osmium.java.schoolconnect.backend.service.UserService;
 import org.springframework.stereotype.Service;
-
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 
 @Service
 public class UserServiceImpl extends MPJBaseServiceImpl<UserMapper, User> implements UserService {
