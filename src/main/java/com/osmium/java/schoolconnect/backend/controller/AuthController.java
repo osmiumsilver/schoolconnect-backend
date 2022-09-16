1c71b155d2f6560a5f4c05a7498c67e102a15385
ZL <1351916203@qq.com>
2022-09-16 21:28:28 +0800

    引入mybatis-plus-join依赖

--- src/main/java/com/osmium/java/schoolconnect/backend/controller/AuthController.java
+++ src/main/java/com/osmium/java/schoolconnect/backend/controller/AuthController.java
@@ -1,68 +1,28 @@
 package com.osmium.java.schoolconnect.backend.controller;
 
-import com.osmium.java.schoolconnect.backend.service.AuthService;
-import com.osmium.java.schoolconnect.backend.service.impl.UserDao;
-import com.osmium.java.schoolconnect.backend.entity.SysUser;
-import com.osmium.java.schoolconnect.backend.utils.Action;
+import com.osmium.java.schoolconnect.backend.entity.User;
+import com.osmium.java.schoolconnect.backend.service.UserService;
+import com.osmium.java.schoolconnect.backend.utils.Result;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
 
 @Controller
 @RequestMapping("/api/auth")
 
-public class AuthController{
-    @Resource
-    AuthService authService;
+public class AuthController {
 
+    @Autowired
+    private UserService userService;
 
-
-    @RequestMapping(value= "/login",method = RequestMethod.POST)
-    private Action login() (@RequestParam("username") String name,
-    @RequestParam("sex") String sex,
-    @RequestParam("grade") String grade,
-    @RequestParam("password") String password) {
-        request.setCharacterEncoding("utf-8");
-
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
-        String level;
-        //实例化UserDao对象
-
-        SysUser sysUser = new UserDao().login(username, Encryptor.MD5Encrypt(password));
-        //判断是否登录成功
-        if (sysUser != null) {//成功
-            level = sysUser.getLevel();
-            if (level.equals("用户")) {
-                request.getSession().setAttribute("user", sysUser);//将用户对象放到session中
-                //转发到user.jsp中
-                response.sendRedirect(request.getContextPath() + "/user.jsp");
-
-                //  request.getRequestDispatcher("user.jsp").forward(request, response);
-            } else {
-                request.getSession().setAttribute("admin", sysUser);//将管理员对象放到session中
-                //转发到admin.jsp中
-                //  request.getRequestDispatcher("admin.jsp").forward(request, response);
-
-                response.sendRedirect(request.getContextPath() + "/admin.jsp");
-            }
-        } else {//失败
-            response.setCharacterEncoding("gb2312");
-            PrintWriter out = response.getWriter();
-            response.setContentType("text/html");
-            out.println("<script charset=\"gb2312\" type=\"text/javascript\">");
-            out.println("alert(' 错误:用户名或密码错误！');");
-            out.println("window.location.href = \"index.html\";");
-            out.println("</script>");
-
-        }
+    @ApiOperation(value = "用户登录", notes = "用户登录")
+    @PostMapping("/login")
+    public Result userLogin(User user) {
+//        userService.get
+        return Result.success();
     }
+
 
 }
--- src/main/java/com/osmium/java/schoolconnect/backend/service/UserService.java
+++ src/main/java/com/osmium/java/schoolconnect/backend/service/UserService.java
@@ -1,5 +1,9 @@
 package com.osmium.java.schoolconnect.backend.service;
 
+import com.github.yulichang.base.MPJBaseService;
+import com.osmium.java.schoolconnect.backend.entity.User;
+import com.osmium.java.schoolconnect.backend.mapper.UserMapper;
+
 import javax.servlet.http.HttpSession;
 
 /**
@@ -7,7 +11,6 @@ import javax.servlet.http.HttpSession;
  * @date 2022/3/29
  * @apinote
  */
-public interface UserService {
-    void register(String name, String grade, String password);
-    User login(HttpSession session);;
+public interface UserService extends MPJBaseService<UserMapper> {
+
 }
